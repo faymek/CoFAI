a0bb56d51537c79d53c9f08b93e90710c88b532e
wangzilong <wangzilong1021@outlook.com>
2026-06-18 12:15:20 +0000

    add MPC_I3 module

--- cofai/models/__init__.py
+++ cofai/models/__init__.py
@@ -1,6 +1,6 @@
-from .mpc import MPC_I1, MPC_I2, MPC_I12, MPC_I12_CtxAsHyper
+from .mpc import MPC_I1, MPC_I2, MPC_I3, MPC_I12, MPC_I12_CtxAsHyper
 from .lamofc import Dinov2TimmOnlyPatchCodec, Dinov2OrigSlideOnlyPatchCodec, Dinov2OrigSlideSegBypass, Dinov2OrigSlideSegVQFC, Dinov2OrigClsVQFC, Dinov2OrigClsBypass, Dinov2TimmSegVQFC
-from .bypass import Dinov2TimmBypass
+from .bypass import Dinov2TimmBypass, Dinov3TimmBypass
 # from .vqfc import Dinov2VQFCCodec  # Skip to avoid mmcv dependency
 
 from .mlore import MLoREFrameCodec, MLoREVideoCodec, MLoREWrapperCodec
@@ -9,6 +9,7 @@ from .orfc import Dinov2ClsORFC, Dinov2SlideSegORFC
 __all__ = [
     "MPC_I1",
     "MPC_I2",
+    "MPC_I3",
     "MPC_I12",
     "MPC_I12_CtxAsHyper",
     "Dinov2TimmOnlyPatchCodec",
@@ -18,6 +19,7 @@ __all__ = [
     "Dinov2OrigClsBypass",
     "Dinov2OrigClsVQFC",
     "Dinov2TimmBypass",
+    "Dinov3TimmBypass",
     "Dinov2TimmSegVQFC",
     # MLoRE/RFC components
     "MLoREFrameCodec",
